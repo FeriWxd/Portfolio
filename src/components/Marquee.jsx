7fe1ad88ba66885f8d90d2759be4dd07c750c3d8
FeriW02 <full.stack020224@gmail.com>
2025-07-10 23:56:24 +0400

    ad

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -5,15 +5,11 @@ const Marquee = () => {
   return (
     <div className="marquee-container">
       <div className="marquee-track">
-        <span>👨‍💻 Ferit — Frontend Developer 💻 ✨ React Lover 🚀 &nbsp;</span>
-        <span>👨‍💻 Ferit — Frontend Developer 💻 ✨ React Lover 🚀 &nbsp;</span>
-        <span>👨‍💻 Ferit — Frontend Developer 💻 ✨ React Lover 🚀 &nbsp;</span>
-        <span>👨‍💻 Ferit — Frontend Developer 💻 ✨ React Lover 🚀 &nbsp;</span>
-        <span>👨‍💻 Ferit — Frontend Developer 💻 ✨ React Lover 🚀 &nbsp;</span>
-        <span>👨‍💻 Ferit — Frontend Developer 💻 ✨ React Lover 🚀 &nbsp;</span>
-        <span>👨‍💻 Ferit — Frontend Developer 💻 ✨ React Lover 🚀 &nbsp;</span>
-        <span>👨‍💻 Ferit — Frontend Developer 💻 ✨ React Lover 🚀 &nbsp;</span>
-        <span>👨‍💻 Ferit — Frontend Developer 💻 ✨ React Lover 🚀 &nbsp;</span>
+        <span>👨‍💻 Ferit Teyubov — Crafting Clean & Scalable Interfaces with React & Tailwind 🚀&nbsp;&nbsp;</span>
+        <span>👨‍💻 Ferit Teyubov — Crafting Clean & Scalable Interfaces with React & Tailwind 🚀&nbsp;&nbsp;</span>
+        <span>👨‍💻 Ferit Teyubov — Crafting Clean & Scalable Interfaces with React & Tailwind 🚀&nbsp;&nbsp;</span>
+        <span>👨‍💻 Ferit Teyubov — Crafting Clean & Scalable Interfaces with React & Tailwind 🚀&nbsp;&nbsp;</span>
+        <span>👨‍💻 Ferit Teyubov — Crafting Clean & Scalable Interfaces with React & Tailwind 🚀&nbsp;&nbsp;</span>
       </div>
     </div>
   );
